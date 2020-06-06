8ccc38feb9ebb8b8188f5b3749c40338dbee4f9a
aeromeror <56206150+aeromeror@users.noreply.github.com>
2020-06-05 21:49:23 -0500

    skere

--- Proyecto_Final/src/MyVisitors.java
+++ Proyecto_Final/src/MyVisitors.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class MyVisitors<T> extends Java8ParserBaseVisitor {
     ArrayList<smell> smells;
     private int methods = 0;
-    //private int GettersAndSetters = 0;
+    private int methodsw = 0;
     private int field = 0;
     private int innerClasses = 0;
     private int innerInterfaces = 0;
@@ -13,14 +13,34 @@ public class MyVisitors<T> extends Java8ParserBaseVisitor {
     @Override
     public T visitClassBody(Java8Parser.ClassBodyContext ctx){
         visitChildren(ctx);
+        double tot=0;
+        if(methods<5){
+            tot=tot+methods*0.20;
+        }else{
+            tot=tot+methods*0.25;
+        }
+        if(field<7){
+            tot=tot+methods*0.10;
+        }else{
+            tot=tot+methods*0.15;
+        }
+        if(innerInterfaces<5){
+            tot=tot+innerInterfaces * 0.25;
+        }else{
+            tot=tot+innerInterfaces * 0.30;
+        }
+        if(innerClasses<5){
+            tot=tot+innerClasses * 0.25;
+        }else{
+            tot=tot+innerClasses * 0.30;
+        }
+        tot=tot/5;
         double largeRate = (methods * 0.24 + field * 0.16 + innerInterfaces * 0.30 + innerClasses * 0.30)/5;
         if(largeRate > 1){
             smells.add(new smell(((Java8Parser.NormalClassDeclarationContext)ctx.parent).start,
                     "This class is to enormous!.\n", "https://refactoring.guru/smells/large-class"));
         }
-        //int logicMethods = methods - GettersAndSetters;
-        int logicMethods = methods;
-        if(logicMethods < 3){
+        if(methods < 3){
             smells.add(new smell(((Java8Parser.NormalClassDeclarationContext)ctx.parent).start,
                     "This class is just a bunch of data!.\n", "https://refactoring.guru/smells/data-class"));
         }
@@ -32,7 +52,7 @@ public class MyVisitors<T> extends Java8ParserBaseVisitor {
             field += ctx.fieldDeclaration().variableDeclaratorList().variableDeclarator().size();
 
         }else if (ctx.methodDeclaration() != null) {
-
+            methodsw+=ctx.methodDeclaration().methodBody().block().blockStatements().blockStatement().size();
             String methodName = ctx.methodDeclaration().methodHeader().methodDeclarator().Identifier().getText();
             boolean isGet = methodName.startsWith("get") || methodName.startsWith("is");
             boolean isSet = methodName.startsWith("set");
