880d6d93d9ce34cdb6b12ae9b60aecbdbd622571
aeromeror <56206150+aeromeror@users.noreply.github.com>
2020-06-06 19:27:07 -0500

    yv7 el campeon

--- Proyecto_Final/src/MyVisitors.java
+++ Proyecto_Final/src/MyVisitors.java
@@ -1,15 +1,16 @@
 import org.antlr.v4.runtime.tree.ParseTree;
+
+import org.antlr.v4.runtime.RuleContext;
 import java.util.ArrayList;
 import java.util.List;
-
 public class MyVisitors<T> extends Java8ParserBaseVisitor {
-    ArrayList<smell> smells;
+    ArrayList<smell> smells=new ArrayList<>();
+    int count = 0;
     private int methods = 0;
     private int methodsw = 0;
     private int field = 0;
     private int innerClasses = 0;
     private int innerInterfaces = 0;
-    private int controlFlow = 0;
 
     @Override
     public T visitClassBody(Java8Parser.ClassBodyContext ctx){
@@ -36,8 +37,8 @@ public class MyVisitors<T> extends Java8ParserBaseVisitor {
             tot=tot+innerClasses * 0.30;
         }
         tot=tot/5;
-        double largeRate = (methods * 0.24 + field * 0.16 + innerInterfaces * 0.30 + innerClasses * 0.30)/5;
-        if(largeRate > 1){
+        System.out.println("total"+tot);
+        if(tot > 1){
             smells.add(new smell(((Java8Parser.NormalClassDeclarationContext)ctx.parent).start,
                     "This class is to enormous!.\n", "https://refactoring.guru/smells/large-class"));
         }
@@ -49,35 +50,46 @@ public class MyVisitors<T> extends Java8ParserBaseVisitor {
     }
     @Override
     public T visitClassMemberDeclaration(Java8Parser.ClassMemberDeclarationContext ctx) {
+
         if(ctx.fieldDeclaration() != null){
             field += ctx.fieldDeclaration().variableDeclaratorList().variableDeclarator().size();
 
         }else if (ctx.methodDeclaration() != null) {
             methodsw+=ctx.methodDeclaration().methodBody().block().blockStatements().blockStatement().size();
             String methodName = ctx.methodDeclaration().methodHeader().methodDeclarator().Identifier().getText();
-            boolean isGet = methodName.startsWith("get") || methodName.startsWith("is");
+            boolean isGet = methodName.startsWith("get");
             boolean isSet = methodName.startsWith("set");
             if(isGet || isSet){
                 //GettersAndSetters++;
             }
             else{
                 methods++;
+                System.out.println("estoy sumando");
             }
         } else if (ctx.classDeclaration() != null) {
             innerClasses++;
         } else if (ctx.interfaceDeclaration() != null) {
             innerInterfaces++;
         }
-        return null;
+        return(T)visitChildren(ctx);
     }
+//---------------------------------------------------------------------
+
+    private int controlFlow = 0;
+
     @Override
     public T visitStatement(Java8Parser.StatementContext ctx) {
+
         controlFlow++;
         return (T) visitChildren(ctx);
     }
+
     @Override
     public T visitMethodBody(Java8Parser.MethodBodyContext ctx) {
         List<Java8Parser.BlockStatementContext> statements = ctx.block().blockStatements().blockStatement();
+        count++;
+        System.out.println("asdads"+count+"tamaño"+statements.size());
+
         String methodName = ((Java8Parser.MethodDeclarationContext)ctx.getParent()).methodHeader().methodDeclarator().Identifier().getText();
         if(statements.size()> 10 ){
             smells.add(new smell(((Java8Parser.MethodDeclarationContext)ctx.parent).start,
@@ -96,4 +108,41 @@ public class MyVisitors<T> extends Java8ParserBaseVisitor {
         }
         return null;
     }
+    //-----------------------------------------------------------------------------
+    public void nestedStatement(RuleContext ctx, int line, int column){
+        RuleContext parentStructure = ctx;
+        String parentString;
+        int nestedCounter = 1;
+        int i;
+        while(true){
+            i = 0;
+            while(parentStructure != null && i < 4){
+                parentStructure = parentStructure.parent;
+                i++;
+            }
+            if(parentStructure != null){
+                parentString = parentStructure.getClass().toString();
+                if(parentString.equals("class Java8Parser$If_elsif_statementContext")){
+                    parentStructure = parentStructure.parent.parent;
+                    parentString = parentStructure.getClass().toString();
+                }
+                if(parentString.equals("class RubyParser$If_statementContext")
+                        || parentString.equals("class RubyParser$Unless_statementContext")
+                        || parentString.equals("class RubyParser$While_statementContext")
+                        || parentString.equals("class RubyParser$For_statementContext")){
+                    nestedCounter++;
+                }
+                if(nestedCounter > 4){
+                    String message = "\nMal olor encontrado, estructura profundamente anidada, Linea: " + line + ", Columna: " + column + "\n"
+                            + "Se recomienda reestructurar la logica del codigo para evitar la complejidad de lectura.\n";
+                   //manager.AddCodeSmell(SMELL.DeeplyNestedCode, line, column, message);
+                    break;
+                }
+            }
+            else{
+                break;
+            }
+        }
+    }
 }
+
