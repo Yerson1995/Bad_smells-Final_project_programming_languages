b58d66af33d6d1a9e3b5c604aedc79e6f8163a64
aeromeror <56206150+aeromeror@users.noreply.github.com>
2020-06-19 20:12:30 -0500

    YMCA

--- Proyecto_Final/src/MyVisitors.java
+++ Proyecto_Final/src/MyVisitors.java
@@ -82,25 +82,25 @@ public class MyVisitors<T> extends Java8ParserBaseVisitor {
     @Override
     public T visitStatement(Java8Parser.StatementContext ctx) {
         //System.out.println("ControlFlow: "+controlFlow);
-            if(ctx.whileStatement()!=null){
-                controlFlow=controlFlow+15;
-                //System.out.println("while");
-            }else if(ctx.forStatement()!=null){
-                controlFlow=controlFlow+10;
-                //System.out.println("for");
-            }else if(ctx.ifThenElseStatement()!=null){
-                controlFlow=controlFlow+8;
-                //System.out.println("if else then");
-            }else if(ctx.ifThenStatement()!=null){
-                controlFlow=controlFlow+5;
-                //System.out.println("if then");
-            }else if(ctx.statementWithoutTrailingSubstatement()!=null){
-                controlFlow++;
-                //System.out.println("statementWithoutTrailing");
-            }else if(ctx.labeledStatement()!=null){
-                controlFlow++;
-                //System.out.println("labeledStatement");
-            }
+        if(ctx.whileStatement()!=null){
+            controlFlow=controlFlow+15;
+            //System.out.println("while");
+        }else if(ctx.forStatement()!=null){
+            controlFlow=controlFlow+10;
+            //System.out.println("for");
+        }else if(ctx.ifThenElseStatement()!=null){
+            controlFlow=controlFlow+8;
+            //System.out.println("if else then");
+        }else if(ctx.ifThenStatement()!=null){
+            controlFlow=controlFlow+5;
+            //System.out.println("if then");
+        }else if(ctx.statementWithoutTrailingSubstatement()!=null){
+            controlFlow++;
+            //System.out.println("statementWithoutTrailing");
+        }else if(ctx.labeledStatement()!=null){
+            controlFlow++;
+            //System.out.println("labeledStatement");
+        }
         return (T) visitChildren(ctx);
     }
 
@@ -214,23 +214,25 @@ public class MyVisitors<T> extends Java8ParserBaseVisitor {
         System.out.println("metodo declarado"+ctx.methodHeader().methodDeclarator().Identifier().toString());
         function f = new function(ctx.methodHeader().methodDeclarator().Identifier().toString(),0, ctx.methodHeader().methodDeclarator().Identifier().getSymbol());
         if(!f.name.equals("main"))
-        tablaFunciones.put(ctx.methodHeader().methodDeclarator().Identifier().toString(),f);
+            tablaFunciones.put(ctx.methodHeader().methodDeclarator().Identifier().toString(),f);
         return (T) visitChildren(ctx);
     }
     @Override
     public T visitMethodDeclarator(Java8Parser.MethodDeclaratorContext ctx) {
         String methodName = ctx.Identifier().toString();
-        String[] parameters = ctx.formalParameterList().getText().split(",");
-        System.out.println("nombre metodo"+methodName);
-        for (int i=0;i >= parameters.length;i++){
-            System.out.println("parametro:"+parameters[i]);
-        }
-        int c = parameters.length;
-        //evalua si tiene mas de 4 parametros
-        if (c > 4){
-            smells.add(new smell(((Java8Parser.MethodDeclarationContext)ctx.parent.parent).start,
-                    "This method have a lot of parameters!.\n"
-                            + "Method name: " + methodName , "https://refactoring.guru/smells/long-parameter-list"));
+        if( ctx.formalParameterList()!=null){
+            String[] parameters = ctx.formalParameterList().getText().split(",");
+            System.out.println("nombre metodo"+methodName);
+            for (int i=0;i < parameters.length;i++){
+                System.out.println("parametro:"+parameters[i]);
+            }
+            int c = parameters.length;
+            //evalua si tiene mas de 4 parametros
+            if (c > 4){
+                smells.add(new smell(((Java8Parser.MethodDeclarationContext)ctx.parent.parent).start,
+                        "This method have a lot of parameters!.\n"
+                                + "Method name: " + methodName , "https://refactoring.guru/smells/long-parameter-list"));
+            }
         }
         return null;
     }
