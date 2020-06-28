3ee3275686d8db18ec7515b450d27188a4b3c524
aeromeror <56206150+aeromeror@users.noreply.github.com>
2020-06-28 16:21:53 -0500

    Yv7 tsukuyomy

--- Proyecto_Final/src/Main.java
+++ Proyecto_Final/src/Main.java
@@ -3,8 +3,37 @@ import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.*;
 import java.io.File;
+import java.util.ArrayList;
 
 public class Main {
+    private static int minimum(int a, int b, int c) {
+        return Math.min(a, Math.min(b, c));
+    }
+
+    public static int computeLevenshteinDistance(String str1, String str2) {
+        return computeLevenshteinDistance(str1.toCharArray(),
+                str2.toCharArray());
+    }
+    private static int computeLevenshteinDistance(char [] str1, char [] str2) {
+        int [][]distance = new int[str1.length+1][str2.length+1];
+
+        for(int i=0;i<=str1.length;i++){
+            distance[i][0]=i;
+        }
+        for(int j=0;j<=str2.length;j++){
+            distance[0][j]=j;
+        }
+        for(int i=1;i<=str1.length;i++){
+            for(int j=1;j<=str2.length;j++){
+                distance[i][j]= minimum(distance[i-1][j]+1,
+                        distance[i][j-1]+1,
+                        distance[i-1][j-1]+
+                                ((str1[i-1]==str2[j-1])?0:1));
+            }
+        }
+        return distance[str1.length][str2.length];
+
+    }
     public static void main(String[] args) throws Exception {
         /*try{
             // crear un analizador léxico que se alimenta a partir de la entrada (archivo  o consola)
@@ -37,6 +66,17 @@ public class Main {
         System.out.println(tree.toStringTree(parser)); // imprime el arbol al estilo LISP
         MyVisitors<Object> loader = new MyVisitors<Object>();
         loader.visit(tree);
+        ArrayList<String> tot= new ArrayList<String>();
+        ArrayList<String> fun = new ArrayList<String>();
+        loader.tablaFunciones.forEach((k,v) -> {
+            fun.add(k);
+            tot.add(k);
+        });
+        ArrayList<String> cla = new ArrayList<String>();
+        loader.tablaClases.forEach((k,v) -> {
+            cla.add(k);
+            tot.add(k);
+        });
         System.out.println("    ");
         loader.tablaFunciones.forEach((k,v) -> {if(v.calls<1){
             loader.smells.add(new smell(v.getT(),
@@ -51,6 +91,18 @@ public class Main {
 
         for (int i = 0; i <loader.smells.size(); i++) {
             System.out.println(loader.smells.get(i).toString());
+        }
+        System.out.println("datos");
+        for(int c =0;c<fun.size();c++){
+            System.out.println("funcion "+fun.get(c));
+        }
+        for(int c =0;c<cla.size();c++){
+            System.out.println("clase "+cla.get(c));
+        }
+        for(int c=0;c<tot.size();c++){
+            for(int d=c+1;d<tot.size();d++){
+                System.out.println("pair "+tot.get(c)+" "+tot.get(d)+" distance "+computeLevenshteinDistance(tot.get(c),tot.get(d)));
+            }
         }
     }
 }
